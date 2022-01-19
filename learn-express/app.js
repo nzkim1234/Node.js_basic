9c6839f82010672dd7a1d1e3ec3c4550810c1d0f
nzkim1234 <nzkim1234@naver.com>
2022-01-19 23:58:30 +0900

    문제 풀이

--- learn-express/app.js
+++ learn-express/app.js
@@ -8,7 +8,7 @@ const path = require('path');
 console.log(process.env) 
 dotenv.config();
 const indexRouter = require('./routes');
-const userRouter = require('/routes/user');
+const userRouter = require('/routers/user');
 
 const app = express();
 app.set('port', process.env.PORT || 3000);
@@ -38,3 +38,60 @@ app.use('/user', userRouter);
 app.use((req, res, next) => {
     res.status(404),send('Not Found');
 });
+
+const multer =require('multer');
+const fs = require('fs');
+
+try {
+    fs.readdirSync('uploads');
+}
+catch (error) {
+    console.error('uploads 폴더가 없어  uploads  폴더를 생성합니다.');
+    fs.mkdirSync('uploads');
+}
+
+const upload = multer({
+    storage: multer.diskStorage({
+        destination(req, file, done) {
+            done(null, 'uploads/');
+        },
+        file(req, file, done) {
+            const ext = path.extname(file.originalname);
+            done(null, path.basename(file.originalname, ext) + Date.now() + ext);
+        },
+    }),
+    limits : { fileSize: 5 * 1024 * 1024},
+});
+
+app.get('/upload', (req, res) => {
+    res.sendfile(path.join(__dirname, 'multipart.html'));
+});
+
+app.post('/upload',
+    upload.fields([{name : 'image1'}, {name : 'image2'}]),
+    (req, res) => {
+        console.log(req.files, req.body);
+        res.send('ok');
+    },
+);
+
+app.use((req, res, next) => {
+    console.log('모든 요청에 다 실행됩니다.');
+    next();
+});
+
+app.get('/', (req, res, next) => {
+    console.log('GET / 요청에서만 실행됩니다.');
+    next();
+}, (req, res) => {
+    throw new Error('에러는 에러 처리 미들웨어로 갑니다.');
+});
+
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500).send(err.message);
+});
+
+app.listen(app.get('port'), () => {
+    console.log(app.get('port'), '번 포트에서 대기 중');
+}); 
