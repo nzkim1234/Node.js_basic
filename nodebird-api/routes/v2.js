947ba6321ed9b777ac1a45a8407d94ebc19b2813
nzkim1234 <nzkim1234@naver.com>
2022-02-10 17:07:09 +0900

    Node.js study

--- nodebird-api/routes/v2.js
+++ nodebird-api/routes/v2.js
@@ -1,34 +1,49 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
+const cors =require('cors');
+const url = require('url');
 
-const { verifyToken, deprecated } = require('./middlewares');
-const { Domain, User, Post, Hashtag } = require('../models');
+const { verifyToken, apiLimiter } = require('./middlewares');
+const { Domain, User, Post, Hashtag} = require('../models');
 
 const router = express.Router();
 
-router.use(deprecated);
+router.use(async (req, res, next) => {
+    const domain = await Domain.findOne({
+        where: {host: url.parse(req.get('origin')).host},
+    });
+    if(domain){
+        cors({
+            origin: req.get('origin'),
+            credentials: true,
+        }) (req, res, next);
+    }
+    else{
+        next();
+    }
+});
 
-router.post('/token', async(req, res) => {
+router.post('/token', apiLimiter, async(req, res) => {
     const { clientSecret } = req.body;
-    try{
+    try {
         const domain = await Domain.findOne({
-            where: { clientSecret },
+            where: {clientSecret},
             include: {
                 model: User,
                 attribute: ['nick', 'id'],
             },
         });
         if(!domain){
-            return res.status(404).json({
-                code:401,
-                message:'등록되지 않은 도메인입니다. 먼저 도메인을 등록하세요',
+            return res.status(401).json({
+                code: 401,
+                message: '등록되지 않은 도메인입니다. 먼저 도메인을 등록하세요.',
             });
         }
         const token = jwt.sign({
             id: domain.User.id,
             nick: domain.User.nick,
         }, process.env.JWT_SECRET, {
-            expiresIn: '1m',
+            expiresIn: '30m',
             issuer: 'nodebird',
         });
         return res.json({
@@ -37,7 +52,7 @@ router.post('/token', async(req, res) => {
             token,
         });
     }
-    catch(error){
+    catch(error) {
         console.error(error);
         return res.status(500).json({
             code: 500,
@@ -46,17 +61,17 @@ router.post('/token', async(req, res) => {
     }
 });
 
-router.get('/test', verifyToken, (req, res) => {
+router.get('/test', verifyToken, apiLimiter, (req, res) => {
     res.json(req.decoded);
 });
 
-router.get('/posts/my', verifyToken, (req, res) => {
-    Post.findAll({where: { userId: req.decoded.id }})
+router.get('/posts/my', apiLimiter, verifyToken, (req, res) => {
+    Post.findAll({where: { userId: req.decoded.id}})
     .then((posts) => {
         console.log(posts);
         res.json({
-            coode: 200,
-            messaage: posts,
+            code: 200,
+            payload: posts,
         });
     })
     .catch((error) => {
@@ -68,12 +83,12 @@ router.get('/posts/my', verifyToken, (req, res) => {
     });
 });
 
-router.get('/posts/hashtag/:title', verifyToken, async (req, res) => {
-    try{ 
-        const hashtag = await Hashtag.findOne({where : {title: req.params.title}});
+router.get('/posts/hashtag/:title', verifyToken, apiLimiter, async(req, res) => {
+    try{
+        const hashtag = await Hashtag.findOne({where: { title: req.params.title}});
         if(!hashtag){
             return res.status(404).json({
-                code:404,
+                code: 404,
                 message: '검색 결과가 없습니다',
             });
         }
@@ -83,7 +98,7 @@ router.get('/posts/hashtag/:title', verifyToken, async (req, res) => {
             payload: posts,
         });
     }
-    catch (error){
+    catch(error) {
         console.error(error);
         return res.status(500).json({
             code: 500,
--- nodecat/routes/index.js
+++ nodecat/routes/index.js
@@ -2,32 +2,58 @@ const express = require('express');
 const axios = require('axios');
 
 const router = express.Router();
+const URL = 'http://localhost:8002/v2';
+axios.defaults.headers.origin = 'http://localhost:4000';
 
-router.get('/test', async(req, res, next) => {
+const request = async (req, api) => {
     try{
-        if (!req.session.jwt) {
-            const tokenResult = await axios.post('http://localhost:8002/v1/token', {
+        if(!req.session.jwt){
+            const tokenResult = await axios.post(`${URL}/token`, {
                 clientSecret: process.env.CLIENT_SECRET,
             });
-            if (tokenResult.data && tokenResult.data.code === 200) {
-                req.session.jwt = tokenResult.data.token;
-            }
-            else{
-                return res.json(tokenResult.data);
-            }
+            req.session.jwt = tokenResult.data.token;
         }
-        const result = await axios.get('http://localhost:8002/v1/test', {
-            headers: {authorization: req.session.jwt},
+        return await axios.get(`${URL}${api}`, {
+            headers: { authorization: req.session.jwt},
         });
-        return res.json(result.data);
     }
     catch(error) {
-        console.error(error);
         if(error.response.status === 419) {
-            return res.json(error.response.data);
+            delete req.session.jwt;
+            return request(req, api);
         }
-        return next(error);
+        return error.response;
     }
+};
+
+router.get('/mypost', async (req, res, next) => {
+    try {
+        const result = await request(req, '/posts/my');
+        res.json(result.data);
+    }
+    catch(error){
+        console.error(error);
+        next(error);
+    }
+});
+
+router.get('/search/:hashtag', async (req, res, next) => {
+    try {
+        const result = await request(
+            req, `/posts/hashtag/${encodeURIComponent(req.params.hashtag)}`,
+        );
+        res.json(result.data);
+    }
+    catch(error){
+        if(error.code){
+            console.error(error);
+            next(error);
+        }
+    }
+});
+
+router.get('/', (req, res) => {
+    res.render('main', {key: process.env.CLIENT_SECRET});
 });
 
 module.exports = router;
