57596655339978526fb60df815cdde1290489a2b
nzkim1234 <nzkim1234@naver.com>
2022-07-13 20:41:40 +0900

    app.js, socket.js add Middleware

--- gif-chat/app.js
+++ gif-chat/app.js
@@ -22,11 +22,22 @@ nunjucks.configure('views', {
 
 connect();
 
+const sessionMiddleware = session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+        httpOnly: true,
+        secure: false,
+    },
+});
+
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.use(cookieParser(process.env.COOKIE_SCRET));
+app.use(sessionMiddleware);
 app.use(session({
     resave: false,
     saveUninitialized: false,
@@ -58,4 +69,4 @@ const server = app.listen(app.get('port'), () => {
     console.log(app.get('port'), '번 포트에서 대기 중');
 });
 
-webSocket(server);
+webSocket(server, app, sessionMiddleware);
--- gif-chat/socket.js
+++ gif-chat/socket.js
@@ -1,11 +1,15 @@
-const SocketIO = require('socket.io')
+const SocketIO = require('socket.io');
+const axios = require('axios');
 
-module.exports = (server, app) => {
+module.exports = (server, app, sessionMiddleware) => {
     const io = SocketIO(server, {path: '/socket.io'});
     app.set('io', io);
     const room = io.of('/room');
     const chat = io.of('/chat');
 
+    io.use((socket, next) => {
+        sessionMiddleware(socket.request, socket.request.res, next);
+    })
     room.on('connection', (socket) => {
         console.group('room 네임스페이스 접속');
         socket.on('disconnect', () => {
@@ -20,10 +24,32 @@ module.exports = (server, app) => {
         const roomId = referer
             .split('/')[referer.split('/').length - 1]
             .replace(/\?.+/, '');
-            socket.join(roomdId);
+            socket.join(roomId);
+            socket.to(roomId).emit('join', {
+                user: 'system',
+                chat: `${req.session.color}님이 입장하셨습니다.`,
+            });
+
             socket.on('disconnect', () => {
                 console.log('chat 네임스페이스 접속 해제');
                 socket.leave(roomId);
+                const currentRoom = socket.adapter.rooms[roomId];
+                const userCount = currentRoom ? currentRoom.length : 0;
+                if(userCount === 0) {
+                    axios.delete(`http://localhost:8005/room/${roomId}`)
+                    .then(() => {
+                        console.log('방 제거 요청 성공');
+                    })
+                    .catch((error) => {
+                        console.error(error);
+                    });
+                }
+                else {
+                    socket.to(roomId).emit('exit', {
+                        user: 'system',
+                        chat: `${req.session.color}님이 퇴장하셨습니다.`,
+                    });
+                }
             });
     });
 };
