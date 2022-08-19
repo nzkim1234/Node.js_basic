cff3d3bf81a6887536ace3dd34d3affc15e2ba46
nzkim1234 <nzkim1234@naver.com>
2022-08-19 22:04:04 +0900

    배포하기

--- nodebird/app.js
+++ nodebird/app.js
@@ -6,7 +6,8 @@ const session = require('express-session');
 const nunjucks = require('nunjucks');
 const dotenv = require('dotenv');
 const passport = require('passport');
-
+const helmet = require('helmet');
+const hpp = require('hpp');
 
 dotenv.config();
 const pageRouter = require('./routes/page');
@@ -15,6 +16,7 @@ const postRouter = require('./routes/post');
 const userRouter = require('./routes/user');
 const { sequelize } = require('./models');
 const passportConfig = require('./passport');
+const logger = require('./logger');
 
 const app = express();
 passportConfig();
@@ -24,7 +26,6 @@ nunjucks.configure('views', {
     express: app,
     watch: true,
 });
-
 sequelize.sync({ force: false})
 .then(() => {
     console.log('데이터베이스 연결 성공');
@@ -33,13 +34,22 @@ sequelize.sync({ force: false})
     console.error(err);
 });
 
-app.use(morgan('dev'));
+if(process.env.NODE_ENV === 'production') {
+    app.use(morgan('combined'));
+    app.use(helmet({ contentSecurityPolicy: false}));
+    app.use(hpp());
+}
+else{
+    app.use(morgan('dev'));
+}
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/img', express.static(path.join(__dirname, 'uploads')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
 app.use(cookieParser(process.env.COOKIE_SECRET));
-app.use(session({
+
+const sessionOption = {
     resave: false,
     saveUninitialized: false,
     secret: process.env.COOKIE_SECRET,
@@ -47,7 +57,13 @@ app.use(session({
         httpOnly: true,
         secure: false,
     },
-}));
+};
+if (process.env.NODE_ENV === 'production') {
+    sessionOption.proxy = true;
+    // sessionOption.cookie.secure = true;
+}
+
+app.use(session(sessionOption));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use('/', pageRouter);
@@ -57,7 +73,8 @@ app.use('/user', userRouter);
 app.use((req, res, next) => {
     const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
     error.status = 404;
-    next(error);
+    logger.info('hello');
+    logger.error(error.message);
 });
 
 app.use((err, req, res, next) => {
